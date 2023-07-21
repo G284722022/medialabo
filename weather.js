1e15be492669b0abd9226b14d9fa8b68dc8856e8
G284722022 <228472@st.takushoku-u.ac.jp>
2023-07-21 23:22:57 +0900

    課題

--- weather.js
+++ weather.js
@@ -1,62 +1,50 @@
-let data = {
-  "coord": {
-    "lon": 116.3972,
-    "lat": 39.9075
-  },
-  "weather": [
-    {
-      "id": 803,
-      "main": "Clouds",
-      "description": "曇りがち",
-      "icon": "04d"
-    }
-  ],
-  "base": "stations",
-  "main": {
-    "temp": 9.94,
-    "feels_like": 8.65,
-    "temp_min": 9.94,
-    "temp_max": 9.94,
-    "pressure": 1022,
-    "humidity": 14,
-    "sea_level": 1022,
-    "grnd_level": 1016
-  },
-  "visibility": 10000,
-  "wind": {
-    "speed": 2.65,
-    "deg": 197,
-    "gust": 4.84
-  },
-  "clouds": {
-    "all": 53
-  },
-  "dt": 1646542386,
-  "sys": {
-    "type": 1,
-    "id": 9609,
-    "country": "CN",
-    "sunrise": 1646520066,
-    "sunset": 1646561447
-  },
-  "timezone": 28800,
-  "id": 1816670,
-  "name": "北京市",
-  "cod": 200
-};
+let b = document.querySelector('#sendRequest');
+b.addEventListener('click', sendRequest);
 
-////////// 課題3-2 ここからプログラムを書こう
-console.log(data.coord.lon);
-console.log(data.coord.lat);
-console.log(data.weather[0].description);
-console.log(data.main.temp_min);
-console.log(data.main.temp_max);
-console.log(data.main.humidity);
-console.log(data.wind.speed);
-console.log(data.wind.deg);
-console.log(data.name);
+function sendRequest(){
+  let id = document.getElementById('id').value.trim();
+  if (id === '') {
+    console.log('都市のidを入力してください');
+    return;
+  }
 
-let p1 = document.querySelector('div#result');
-p1.textContent = ("緯度: "+data.coord.lon);
-let p2 = document.querySelector('div#result');
-p2.textContent = ("都市名: "+data.name);
+  let url = 'https://www.nishita-lab.org/web-contents/jsons/openweather/' + id + '.json';
+
+  axios.get(url)
+  .then(showResult)   // 通信成功
+  .catch(showError)   // 通信失敗
+  .then(finish);      // 通信の最後の処理
+}
+
+function showResult(resp){
+  let data = resp.data;
+  if (typeof data === 'string'){
+    data = JSON.parse(data);
+
+  }
+  let p = document.querySelector('p#lat');
+  p.textContent = ("緯度: "+data.coord.lat);
+  let p1 = document.querySelector('p#lon');
+  p1.textContent = ("経度: "+data.coord.lon);
+  let p3 = document.querySelector('p#weather');
+  p3.textContent = ("天気: "+data.weather[0].description);
+  let p4 = document.querySelector('p#temp_max');
+  p4.textContent = ("最高気温: "+data.main.temp_max+"℃");
+  let p5 = document.querySelector('p#temp_min');
+  p5.textContent = ("最低気温: "+data.main.temp_min+"℃");
+  let p6 = document.querySelector('p#situ');
+  p6.textContent = ("湿度: "+data.main.humidity+"%");
+  let p7 = document.querySelector('p#wind_speed');
+  p7.textContent = ("風速: "+data.wind.speed+"m/s");
+  let p8 = document.querySelector('p#wind_deg');
+  p8.textContent = ("風向: "+data.wind.deg);
+  let p9 = document.querySelector('p#name');
+  p9.textContent = ("都市名: "+data.name); 
+}
+  function showError(err) {
+    console.log(err);
+}
+
+function finish() {
+  console.log('Ajax 通信が終わりました');
+}
